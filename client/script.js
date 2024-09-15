441e383cde6d5609c5a6b95ac16634f0b5c5fb44
harrisonstark <harrisonstarkshs@gmail.com>
2024-09-15 15:22:12 -0700

    video

--- client/script.js
+++ client/script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $('.car-button').click(function() {
         const buttonId = $(this).attr('id');
         $.ajax({
-            url: `http://localhost:7171/push_event?event=${buttonId}`,
+            url: `https://gtr-pi-car.harrisonstark.net/push_event?event=${buttonId}`,
             method: 'POST',
             success: function(response) {
                 console.log(`Action for ${buttonId} was successful, ${JSON.stringify(response)}`);
